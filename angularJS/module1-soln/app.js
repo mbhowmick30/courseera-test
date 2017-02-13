fc1f7608c8585887f66a598609d6493556f6b866
mbhowmick30 <mbhowmick30@gmail.com>
2017-02-13 15:16:01 -0500

    Update app.js

--- angularJS/module1-soln/app.js
+++ angularJS/module1-soln/app.js
@@ -1,1 +1,28 @@
 
+(function(){
+
+'use strict';
+
+angular.module('lunchApp',[])
+		.controller('LunchController', LunchController);
+
+LunchController.$inject = ['$scope'];		
+function LunchController($scope) {
+
+	$scope.lunchMenu = "";
+	$scope.message ="";
+	$scope.listLunchMenu= function (){
+		var lunchMenu = $scope.lunchMenu;
+		var items = lunchMenu.split(",");
+		console.log(items);
+		if (items.length>3)
+			$scope.message = "too much food";
+		else
+			$scope.message = "Good Lunch";
+	};	
+
+}
+
+})();
+
+
