8a4d0ec9476a7d3dc0c97893cdc1d37d23788f72
mbhowmick30 <mbhowmick30@gmail.com>
2017-06-26 14:35:56 -0400

    Update app.js

--- angularJS/module3-soln/app.js
+++ angularJS/module3-soln/app.js
@@ -68,7 +68,7 @@ function MenuSearchService($http, ApiBasePath) {
     return response.then(function(result){
 
       // process result and only keep items that match
-      var foundItems = results;
+      var foundItems = result;
 
       // return processed items
       return foundItems;
