5da3722dd2642725082c1bc7733e4bf6502f92c1
mbhowmick30 <mbhowmick30@gmail.com>
2017-06-29 15:44:34 -0400

    Add files via upload

---
+++ angularJS/module4-soln/src/menudata.service.js
@@ -0,0 +1,25 @@
+(function(){
+
+'use strict'
+
+    angular.module('Data')
+    .service('MenuDataService', MenuDataService);
+
+    function MenuDataService(){
+
+      var service = this;
+
+      service.getAllCategories = function(){
+
+      };
+
+      service.getItemsForCategory= function(categoryShortName){
+
+      };
+
+
+
+
+    }
+
+})();
