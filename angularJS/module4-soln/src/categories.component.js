2a832a6e357d9c34110ce9f2b0622720675ab085
mbhowmick30 <mbhowmick30@gmail.com>
2017-06-30 14:41:54 -0400

    Add files via upload

--- angularJS/module4-soln/src/categories.component.js
+++ angularJS/module4-soln/src/categories.component.js
@@ -2,5 +2,26 @@
 'use strict'
 
     angular.module('Data')
+    .component('categories', {
+
+        templateUrl: "src/categories.template.html",
+        controller: CategoriesComponentController,
+        bindings: {
+          categories: '<'
+        }
+    });
+
+    CategoriesComponentController.$inject = ['$rootScope','$element','$q', '$MenuDataService'];
+    function CategoriesComponentController ($rootScope,$element,$q, $MenuDataService){
+
+      $ctrl = this;
+      $ctrl.$doCheck= function (){
+
+
+
+      };
+
+    }
+
 
 })();
--- angularJS/module4-soln/src/menudata.service.js
+++ angularJS/module4-soln/src/menudata.service.js
@@ -10,10 +10,38 @@
       var service = this;
 
       service.getAllCategories = function(){
+        var response = $http({
+          method: "GET",
+          url: ("https://davids-restaurant.herokuapp.com/categories.json"),
+        });
+
+        return response.then(function(result){
+          console.log("categories = "+result.data);
+          var data = result.data.categories;
+          return data;
+
+        }).catch(function(error){
+          console.log(error);
+        });
 
       };
 
       service.getItemsForCategory= function(categoryShortName){
+
+        var response = $http({
+          method: "GET",
+          url: ("https://davids-restaurant.herokuapp.com/menu_items.json?category="+categoryShortName),
+        });
+
+        return response.then(function(result){
+          console.log("Gargi here "+result.data);
+          var data = result.data;
+          return data;
+
+        }).catch(function(error){
+          console.log(error);
+        });
+
 
       };
 
