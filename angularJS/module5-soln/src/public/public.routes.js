d0ddbf1297cddd2ff383694a325031b614f7c6ed
mbhowmick30 <mbhowmick30@gmail.com>
2017-07-07 13:14:43 -0400

    Update public.routes.js

--- angularJS/module5-soln/src/public/public.routes.js
+++ angularJS/module5-soln/src/public/public.routes.js
@@ -41,5 +41,19 @@ function routeConfig ($stateProvider) {
         }]
       }
     });
+  
+  .state('public.info', {
+    url: '/info',
+    templateUrl: 'src/public/info/info.html',
+    controller: 'InfoController',   
+    controllerAs: 'infoCtrl'
+  })
+
+  .state('public.signup', {    
+    url: '/signup',
+    templateUrl: 'src/public/signup/signup.html',
+    controller: 'SignupController',
+    controllerAs: 'signupCtrl'
+  });
 }
 })();
